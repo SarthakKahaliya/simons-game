5abc7a6a2d0c623d5bbd6a49284883ee3196a50b
SarthakKahaliya <sarthakkahaliya@gmail.com>
2022-05-28 17:30:08 -0400

    New Features
    1. Sound can be Toggled on and Off.
    2. If incorrect answer, if Hints remaining, then instead of game over it will show the hint and auto level up.
    3. After every 5 levels Hint will increase by 1.

--- game.js
+++ game.js
@@ -19,10 +19,13 @@ var userClickedPattern = [];
 
 var started = 0;
 
-var level = 0;
+var level = 1;
 
-var hintAvailable = 3;
+var hintAvailable = 2;
 
+var soundToggle = 1;
+
+// Instructions
 $(".instructions-para").hide();
 
 $(".instructions-button").on("click", function () {
@@ -32,8 +35,12 @@ $(".instructions-button").on("click", function () {
   $(".instructions-para").slideToggle();
 });
 
+// Create the Next Sequence
 function nextSequence() {
-  $("h1").text("Level " + ++level);
+  $("h1").text("Level " + level);
+  if(level%5==0){
+    hintAvailable++;
+  }
   $(".show-pattern")
     .css("visibility", "visible")
     .text("Show Order (" + hintAvailable + ")");
@@ -53,31 +60,31 @@ function nextSequence() {
   gamePattern.push(randomChosenColor);
 }
 
+// Detecting the Keyboard input event
 $(document).on("keydown", function (event) {
-    if (started === 0) {
-      nextSequence();
-      started = 1;
-    } else {
-      if (keyboardKeys.includes(event.key)) {
-        if (event.key == "ArrowUp" || event.key == "w") {
-          userChosenColor = "green";
-        } else if (event.key == "ArrowLeft" || event.key == "a") {
-          userChosenColor = "yellow";
-        } else if (event.key == "ArrowRight" || event.key == "d") {
-          userChosenColor = "blue";
-        } else if (event.key == "ArrowDown" || event.key == "s") {
-          userChosenColor = "red";
-        }
-        userClickedPattern.push(userChosenColor);
-        check();
-        animatePress(userChosenColor);
-        playAudio(userChosenColor);
+  if (started === 0) {
+    nextSequence();
+    started = 1;
+  } else {
+    if (keyboardKeys.includes(event.key)) {
+      if (event.key == "ArrowUp" || event.key == "w") {
+        userChosenColor = "green";
+      } else if (event.key == "ArrowLeft" || event.key == "a") {
+        userChosenColor = "yellow";
+      } else if (event.key == "ArrowRight" || event.key == "d") {
+        userChosenColor = "blue";
+      } else if (event.key == "ArrowDown" || event.key == "s") {
+        userChosenColor = "red";
       }
+      userClickedPattern.push(userChosenColor);
+      check();
+      animatePress(userChosenColor);
+      playAudio(userChosenColor);
     }
-  });
+  }
+});
 
-
-
+// Detecting the Clicking input event
 $(".btn").on("click", function () {
   if (started === 0) {
     nextSequence();
@@ -91,12 +98,14 @@ $(".btn").on("click", function () {
   }
 });
 
+// Detecting the Clicking on Show Pattern button
 $(".show-pattern").on("click", function () {
   if (started == 1) {
     showPattern();
   }
 });
 
+// Function to Show the pattern
 function showPattern() {
   if (hintAvailable > 0) {
     var i = 0;
@@ -109,10 +118,9 @@ function showPattern() {
   }
 }
 
+// Looping Function for Delaying pattern occurance
 function myLoop(i) {
-  //  create a loop function
   setTimeout(function () {
-    //  call a 3s setTimeout when the loop is called
     $("#" + gamePattern[i])
       .fadeOut(100)
       .fadeIn(100); //  your code here
@@ -120,24 +128,28 @@ function myLoop(i) {
     if (i < gamePattern.length) {
       //  if the counter < 10, call the loop function
       myLoop(i); //  ..  again which will trigger another
+    }else if(i == gamePattern.length){
+        levelUp();
     } //  ..  setTimeout()
-  }, 600);
+  }, 1000);
 }
 
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
+// Checking if the User input is Correct
 function check() {
   if (
     userClickedPattern[userClickedPattern.length - 1] !=
     gamePattern[userClickedPattern.length - 1]
   ) {
-    gameOver();
+    if (hintAvailable > 0) {
+      playAudio("wrong");
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+      showPattern();
+    } else {
+      gameOver();
+    }
   } else {
     if (userClickedPattern.length == gamePattern.length) {
       levelUp();
@@ -145,14 +157,17 @@ function check() {
   }
 }
 
+// Function to Level Up
 function levelUp() {
   setTimeout(function () {
     playAudio("levelup");
     userClickedPattern = [];
+    level++;
     nextSequence();
   }, 500);
 }
 
+// Function when Game Over
 function gameOver() {
   $("h1").html(
     "<span style = 'color: red;'>Game Over!</span> Press/Click Any Key to Restart"
@@ -164,12 +179,13 @@ function gameOver() {
   playAudio("wrong");
   $(".show-pattern").css("visibility", "hidden");
   started = 0;
-  level = 0;
+  level = 1;
   gamePattern = [];
   userClickedPattern = [];
   hintAvailable = 3;
 }
 
+// Function Animating the Clicking of the Button
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
@@ -177,7 +193,21 @@ function animatePress(currentColor) {
   }, 100);
 }
 
+// Function controlling the audio
 function playAudio(currentColor) {
-  var sound = new Audio("sounds/" + currentColor + ".mp3");
-  sound.play();
+  if (soundToggle == 1) {
+    var sound = new Audio("sounds/" + currentColor + ".mp3");
+    sound.play();
+  }
 }
+
+// Function to Toggle Sound
+$(".sound").on("click", function () {
+  if (soundToggle == 1) {
+    soundToggle = 0;
+    $(".sound").html("<i class='fa-solid fa-volume-xmark'>");
+  } else if (soundToggle == 0) {
+    soundToggle = 1;
+    $(".sound").html("<i class='fa-solid fa-volume-high'></i>");
+  }
+});
